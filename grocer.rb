7b68a8ef94429760585221a8c88fae4774db1150
Rick Glascock <rixong@gmail.com>
2019-10-24 11:42:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,19 +13,18 @@ def consolidate_cart(items)
 return cart
 end
 
-def consolidate_cart(items)
-  cart = {}
-  items.map do |item|
-    item.each_pair do |(key,value)|
-      if !cart.has_key?(key)
-        cart[key] = value
-        cart[key][:count] = 1
-      else 
-        cart[key][:count] +=1
+def apply_coupons(coupons, cart) ## assumes coupons can ONLY be used if buying at least number indicated on coupon
+  coupons.map do |coupon|
+    item = coupon[:item]
+    if cart.has_key?(item) && cart[item][:count] >= coupon[:num]
+      cart["#{item} W/COUPON"] = {:price => coupon[:cost]/coupon[:num], :clearance => cart[item][:clearance], :count => coupon[:num]}
+      cart[item][:count] -= coupon[:num]
+      if cart[item][:count] == 0
+        cart.delete(item)
       end
     end
   end
-return cart
+  return cart
 end
 
 def apply_clearance (cart)
